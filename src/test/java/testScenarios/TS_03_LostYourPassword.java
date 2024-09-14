2f68debd02cbdac5f4de1cf3dec2ab1fd605295f
krupaenagantik <krupaenagantik@krupas-mbp.lan>
2024-09-13 20:15:23 -0400

    Krupa LostYourPassword

--- src/test/java/testScenarios/TS_03_LostYourPassword.java
+++ src/test/java/testScenarios/TS_03_LostYourPassword.java
@@ -11,6 +11,16 @@ public class TS_03_LostYourPassword {
 
 	@Test
 	public void f() throws Exception {
+		 WebDriver driver;
+	      driver = new ChromeDriver();
+	      driver.manage().window().maximize();
+	      Thread.sleep(5000);
+	      driver.get("https://practice.automationtesting.in");
+	      //driver.findElement(By.linkText("Practice site")).click();
+	      driver.findElement(loc.myAccount).click();
+	      driver.findElement(loc.lostYourPassword).click();
+		  driver.findElement(loc.lostPassword_UserName).sendKeys("test12345@gmail.com");
+		  driver.findElement(loc.lostPassword_ResetButton).click();
 		
 	}
 }
